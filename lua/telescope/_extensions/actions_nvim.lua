c599dc94765287b7538c312331c593e825e7094f
yaanae <103531280+yaanae@users.noreply.github.com>
2023-02-18 15:19:52 +0100

    Now working again.

--- lua/telescope/_extensions/actions_nvim.lua
+++ lua/telescope/_extensions/actions_nvim.lua
@@ -7,31 +7,31 @@ local action_state = require("telescope.actions.state")
 local utils = require("actions_nvim.utils")
 
 local populate = function(commands)
-  print(vim.inspect(commands))
+  -- print(vim.inspect(commands))
   local Terminal = require("toggleterm.terminal").Terminal
   local result = {}
   for _, val in ipairs(commands) do
-    print(vim.inspect(val))
-    print(type(val.cmd))
+    -- print(vim.inspect(val))
+    -- print(type(val.cmd))
     if type(val.cmd) == "string" then
-      print("Command is a string")
+      -- print("Command is a string")
       table.insert(result, {
         name = val.name,
         cmd = function()
-          -- print("Called function")
+          -- -- print("Called function")
           local win = vim.api.nvim_get_current_win()
           local expanded_cmd = utils.expand_cmd(val.cmd, win, { env = false })
-          print(vim.inspect(expanded_cmd))
+          -- print(vim.inspect(expanded_cmd))
           local term = Terminal:new({
             cmd = expanded_cmd,
             close_on_exit = false,
           })
-          -- print(vim.inspect(term))
+          -- -- print(vim.inspect(term))
           term:toggle()
         end,
       })
     else
-      print("Command is a function")
+      -- print("Command is a function")
       table.insert(result, { name = val.name, cmd = val.cmd })
     end
   end
@@ -40,8 +40,8 @@ local populate = function(commands)
 end
 
 local picker = function(opts, commands)
-  print("In picker, commands")
-  print(vim.inspect(commands))
+  -- print("In picker, commands")
+  -- print(vim.inspect(commands))
   opts = opts or {}
   pickers
       .new(opts, {
@@ -49,8 +49,8 @@ local picker = function(opts, commands)
         finder = finders.new_table({
           results = commands,
           entry_maker = function(entry)
-            print("In picker, entry_maker, entry")
-            print(vim.inspect(entry))
+            -- print("In picker, entry_maker, entry")
+            -- print(vim.inspect(entry))
             return {
               value = entry,
               display = entry.name,
@@ -63,7 +63,7 @@ local picker = function(opts, commands)
           actions.select_default:replace(function()
             actions.close(prompt_bufnr)
             local selection = action_state.get_selected_entry()
-            -- print(vim.inspect(selection))
+            -- -- print(vim.inspect(selection))
             -- These curly braces are required.
             -- vim.api.nvim_put({ selection.value[1] }, "", false, true)
             selection.value.cmd()
@@ -75,20 +75,20 @@ local picker = function(opts, commands)
 end
 
 local function run(opts)
-  print("Inspect Opts, in run")
-  print(vim.inspect(opts))
-  print("Inspect get_actions, in run")
-  print(vim.inspect(get_actions))
+  -- print("Inspect Opts, in run")
+  -- print(vim.inspect(opts))
+  -- print("Inspect get_actions, in run")
+  -- print(vim.inspect(get_actions))
   local config_actions = get_actions()
-  print("Inspect get_actions return, in run")
-  print(vim.inspect(config_actions))
+  -- print("Inspect get_actions return, in run")
+  -- print(vim.inspect(config_actions))
   picker(opts, populate(config_actions))
 end
 
 return require("telescope").register_extension({
   setup = function(config)
-    print("Inspect Config")
-    print(vim.inspect(config))
+    -- print("Inspect Config")
+    -- print(vim.inspect(config))
     get_actions = config.get_actions
   end,
   exports = {
